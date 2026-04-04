5e11cf5008724eff0d5ac503c96ea6f85b7aacdd
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-04-04 14:02:37 +0700

    add all page needed

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import Login from "./pages/auth/Login.jsx";
 import Dashboard from "./pages/Dashboard/Index.jsx";
 import Register from "./pages/auth/Register.jsx";
 import Transfer from "./pages/Dashboard/Transfer.jsx";
+import History from "./pages/Dashboard/History.jsx";
+import TopUp from "./pages/Dashboard/TopUp.jsx";
+import Profile from "./pages/Dashboard/Profile.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -19,6 +22,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/transfer" element={<Transfer />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/topup" element={<TopUp />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   </BrowserRouter>,
 );
